2ac68b0c9513a5af1222b2aa83315a122051cc55
Federico Baez <federicko94@gmail.com>
2022-11-29 21:09:41 -0300

    add video preview.

---
+++ JS/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+const video = document.getElementById('video');
+video.load();
+
+function play() {
+    video.play();
+}
+
+function pause() {
+    video.pause()
+}
